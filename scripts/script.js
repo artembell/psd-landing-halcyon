e16dcd809dbf6bb0116ff9e7a816b232d6c2ecaa
artembell <artembell305@gmail.com>
2018-07-04 20:21:11 +0300

    Fully working hiding/showing animations

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,27 @@ $(document).ready(function(){
 		startSlide: 0
 	});	
 
-	$(".close").bind("click", function(){
-		$("#shit").attr('src') == "img/close-btn-up.png"
-		? $("#shit").attr("src","img/close-btn-down.png")
-		: $("#shit").attr("src","img/close-btn-up.png")
+	//пока будет привязано 2 разных события на 2 разные кнопки
+	//позже надо объединить в одно
+
+	$(".close1").bind("click", function(){
+		if($("#shit1").attr('src') == "img/close-btn-up.png"){
+			$("#shit1").attr("src","img/close-btn-down.png");
+			//alert("change on DOWN");
+		}else{
+			$("#shit1").attr("src","img/close-btn-up.png");
+			//alert("change on UP");
+		}
+	});
+
+	$(".close2").bind("click", function(){
+		if($("#shit2").attr('src') == "img/close-btn-up.png"){
+			$("#shit2").attr("src","img/close-btn-down.png");
+			//alert("change on DOWN");
+		}else{
+			$("#shit2").attr("src","img/close-btn-up.png");
+			//alert("change on UP");
+		}
 	});
 });
 
